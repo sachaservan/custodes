ef67dd8152401db6b853c29ff5f6b53bf811a930
Ogisan <sachaservanschreiber@gmail.com>
2018-04-19 17:33:22 -0400

    add script to gen datasets

---
+++ datasetgen.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import csv
+import sys
+import math
+import random
+
+def convert(outfile, n, valmin, valmax):
+    with open(outfile, 'w') as fout:
+        for i in range(0, n):
+            r1 = random.randint(valmin, valmax)
+            r2 = random.randint(valmin, valmax)
+
+            fout.write("" + str(r1) + "," + str(r2) + "\n")
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 5):
+        print ("Create a random dataset with two cols and <n> rows."\
+                "where values is between <min> and <max>.")
+        print ("Usage: datasetgen.py <file out> <n> <min> <max>")
+        sys.exit(1)
+
+    sys.argv.pop(0)
+    fout = sys.argv.pop(0)
+    n = sys.argv.pop(0)
+    minval = sys.argv.pop(0)
+    maxval = sys.argv.pop(0)
+    convert(fout, int(n), int(minval), int(maxval))
