631c8ba1759d93bf8d6ff7710bce7a4340ff5751
Ogisan <sachaservanschreiber@gmail.com>
2018-06-02 10:51:04 -0400

    add scripts for generating benchmark datasets

---
+++ chidatagen.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import csv
+import sys
+import math
+import random
+
+def convert(outfile, n, k):
+    with open(outfile, 'w') as fout:
+        for i in range(0, n):
+            r = random.randint(0, k+1)
+            for j in range(0, k-1):
+                if j == r:
+                    fout.write(str(1) + ",")
+                else:
+                    fout.write(str(0) + ",")
+            if r == k:
+                fout.write(str(1) + "\n")
+            else:
+                fout.write(str(0) + "\n")
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 4):
+        print ("Create a random categorical dataset with <k> cols (categories) \
+                and <n> rows.")
+        print ("Usage: datasetgen.py <file out> <n> <k>")
+        sys.exit(1)
+
+    sys.argv.pop(0)
+    fout = sys.argv.pop(0)
+    n = sys.argv.pop(0)
+    k = sys.argv.pop(0)
+    convert(fout, int(n) + 1, int(k) + 1)
