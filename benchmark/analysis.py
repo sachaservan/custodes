c7e8a1ef8fa96e2c041d190300732edc96dcb011
ezg <emanuel.zgraggen@gmail.com>
2018-06-06 14:48:47 -0400

    updated results

--- benchmark/analysis.py
+++ benchmark/analysis.py
@@ -1,26 +1,8 @@
-from enum import Enum
-class TestType(Enum):
-    T_TEST = 1
-    PEARSON = 2
-    CHI2 = 3
-    NONE = 4
+import glob
+import json
 
+for filename in glob.glob("../cmd/*.json"):
+    with open(filename) as f:
+        data = json.load(f)
+        print(data)
 
-with open('../log.txt') as f:
-    content = f.readlines()
-
-content = [x.strip() for x in content] 
-
-
-current_test_type = TestType.NONE
-
-for i, line in enumerate(content):
-    if line == 'Running T-Test...':
-        current_test_type = TestType.T_TEST
-    elif line == 'Running Pearson\'s Coorelation Test...':
-        current_test_type = TestType.PEARSON  
-    elif line == 'Running Chi^2 Test...':
-        current_test_type = TestType.CHI2        
-    else:
-        print(line.split('\t'))
-        
