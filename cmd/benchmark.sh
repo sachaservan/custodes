c62126707f9ef911667a6289810dcf48bd18bf07
ezg <emanuel.zgraggen@gmail.com>
2018-06-07 11:10:24 -0400

    updated results

--- cmd/benchmark.sh
+++ cmd/benchmark.sh
@@ -1,7 +1,8 @@
 make
 
-./hypocert -parties 3 -threshold 2 -rootdir "/data/ez/go/src/hypocert" -netlat 0
-./hypocert -parties 4 -threshold 3 -rootdir "/data/ez/go/src/hypocert" -netlat 0
-./hypocert -parties 8 -threshold 5 -rootdir "/data/ez/go/src/hypocert" -netlat 0
-./hypocert -parties 16 -threshold 9 -rootdir "/data/ez/go/src/hypocert" -netlat 0
-./hypocert -parties 32 -threshold 17 -rootdir "/data/ez/go/src/hypocert" -netlat 0
+for i in {0..5} ; do
+    echo "$i"
+    ./hypocert -parties 4 -threshold 2 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i"
+    ./hypocert -parties 8 -threshold 4 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i"
+    ./hypocert -parties 16 -threshold 8 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i"
+done
