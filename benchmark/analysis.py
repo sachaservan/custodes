061782e09f603c4da82dc7246236fe28ffbfcfeb
ezg <emanuel.zgraggen@gmail.com>
2018-06-07 17:30:18 -0400

    updated

--- benchmark/analysis.py
+++ benchmark/analysis.py
@@ -47,12 +47,13 @@ sns.set(context='paper', style={'axes.axisbelow': True,
 flatui = ['#28aad5', '#b24d94', '#38ae97' ,'#ec7545']
 
 
-def runtime_bar(type, data, shares, show):
+def runtime_bar(type, data, shares, showCategoriesLabel, show):
     comptime = {}
     divtime = {}
     
     numParties = []
     datasetSizes = []
+    categories = []
     
     #prefill
     for d in data:
@@ -65,44 +66,67 @@ def runtime_bar(type, data, shares, show):
             if d['NumberOfParties'] not in numParties:
                 numParties.append(d['NumberOfParties'])
                 
+            if d['NumberOfCategories'] not in categories:
+                categories.append(d['NumberOfCategories'])
+                
             if d['NumberOfParties'] not in comptime[d['DatasetSize']]:
-                comptime[d['DatasetSize']][d['NumberOfParties']] = []
-                divtime[d['DatasetSize']][d['NumberOfParties']] = []               
+                comptime[d['DatasetSize']][d['NumberOfParties']] = {}
+                divtime[d['DatasetSize']][d['NumberOfParties']] = {}  
+
+            if d['NumberOfCategories'] not in comptime[d['DatasetSize']][d['NumberOfParties']]:
+                comptime[d['DatasetSize']][d['NumberOfParties']][d['NumberOfCategories']] = []
+                divtime[d['DatasetSize']][d['NumberOfParties']][d['NumberOfCategories']] = []   
        
     for d in data:
         if d['TestType'] == type:
-            print(d['DatasetSize'], d['NumberOfParties'])
-            comptime[d['DatasetSize']][d['NumberOfParties']].append(d['ComputationTime'])
-            divtime[d['DatasetSize']][d['NumberOfParties']].append(d['DivisionTime'])
+            comptime[d['DatasetSize']][d['NumberOfParties']][d['NumberOfCategories']].append(d['ComputationTime'])
+            divtime[d['DatasetSize']][d['NumberOfParties']][d['NumberOfCategories']].append(d['DivisionTime'])
             
-            
+    print(numParties, datasetSizes)        
     width = 0.15    
     gap = 0.01
     f, (ax1) = plt.subplots(1, 1, sharey=False, figsize=(6, 4))             
     numParties = sorted(numParties)
     datasetSizes = sorted(datasetSizes)
+    categories = sorted(categories)
     
     xIndices = len(numParties) * len(datasetSizes)  
     xTicks = []
     xLabels = []
     p1 = None
     p2 = None
-    for ds, datasetSize in enumerate(datasetSizes):
-        for p, numParty in enumerate(numParties):        
-            mean_comp = np.array(comptime[datasetSize][numParty]).mean() 
-            std_comp = np.array(comptime[datasetSize][numParty]).std()
-            mean_div = np.array(divtime[datasetSize][numParty]).mean()
-            std_div = np.array(divtime[datasetSize][numParty]).std()
-            #current_in
-            total_width = width * len(numParties) + gap * (len(numParties) - 1)
-            pos = (ds - total_width / 2 + width / 2) + width * p + gap * p
-            xTicks.append(pos)
-            if p == (len(numParties) - 1) / 2:
-                xLabels.append(str(numParty) + '\n' + '{:,}'.format(datasetSize))
-            else:
+    for ds, datasetSize in enumerate(datasetSizes): 
+        if showCategoriesLabel:
+            datasetLabelPrefix = '\n\n'
+        # dataset label
+        xTicks.append(ds * len(categories))
+        
+        xLabels.append(datasetLabelPrefix + 'rows: {:,}'.format(datasetSize))    
+        for c, category in enumerate(categories):  
+            idx = ds * len(categories) + c 
+            
+            datasetLabelPrefix = '\n'
+            if showCategoriesLabel:
+                # categories label
+                xTicks.append(idx)
+                xLabels.append('\n' + 'categories: {:,}'.format(category))            
+            
+            
+            for p, numParty in enumerate(numParties):        
+                mean_comp = np.array(comptime[datasetSize][numParty][category]).mean() 
+                std_comp = np.array(comptime[datasetSize][numParty][category]).std()
+                mean_div = np.array(divtime[datasetSize][numParty][category]).mean()
+                std_div = np.array(divtime[datasetSize][numParty][category]).std()
+                #current_in
+                total_width = width * len(numParties) + gap * (len(numParties) - 1)
+                pos = (idx - total_width / 2 + width / 2) + width * p + gap * p
+                
+                # num party label
+                xTicks.append(pos)
                 xLabels.append(numParty)
-            p1 = ax1.bar(pos, mean_comp, width, yerr=std_comp, color=flatui[0])            
-            p2 = ax1.bar(pos, mean_div, width, bottom=mean_comp, yerr=std_div, color=flatui[1], hatch='//')
+                
+                p1 = ax1.bar(pos, mean_comp, width, yerr=std_comp, color=flatui[0])            
+                p2 = ax1.bar(pos, mean_div, width, bottom=mean_comp, yerr=std_div, color=flatui[1], hatch='//')
           
     plt.xticks(xTicks, xLabels)
     ax1.xaxis.grid(False)
@@ -110,7 +134,10 @@ def runtime_bar(type, data, shares, show):
     ax1.yaxis.set_major_formatter(plt.FuncFormatter(minutes_second_formatter))
     plt.legend((p1[0], p2[0]), ('computation', 'division'))
     
-    f.savefig('fig/runtime_' + type.lower() + '.pdf', bbox_inches='tight')
+    mode = 'noshares'
+    if shares:
+        mode = 'shares'
+    f.savefig('fig/runtime_' + type.lower() + mode + '.pdf', bbox_inches='tight')
     plt.tight_layout()
     
     if show:
@@ -120,7 +147,7 @@ def runtime_bar(type, data, shares, show):
     
 
 if __name__== "__main__":
-    show = False
+    show = True
     
     all_runs = []
     for filename in glob.glob("./res/*.json"):
@@ -128,7 +155,6 @@ if __name__== "__main__":
             data = json.load(f)
             all_runs.append(data)
             
-    runtime_bar('TTEST', all_runs, false, show)
-    runtime_bar('PEARSON', all_runs, false, show)
+    runtime_bar('CHI2', all_runs, True, True, show)
         
         
