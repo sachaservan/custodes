113216c199e59575ef961a89d51e79b43a820e96
ezg <emanuel.zgraggen@gmail.com>
2018-06-21 21:41:51 -0400

    update

--- benchmark/benchmark_shares.sh
+++ benchmark/benchmark_shares.sh
@@ -4,13 +4,9 @@ make
 cd ../benchmark
 
 
-for i in {0..1} ; do
+for i in {0..5} ; do
     echo "$i"
     ../cmd/hypocert -parties 4 -threshold 2 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares
     ../cmd/hypocert -parties 8 -threshold 4 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares
     ../cmd/hypocert -parties 16 -threshold 8 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares
-    ../cmd/hypocert -parties 32 -threshold 16 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares
-    ../cmd/hypocert -parties 64 -threshold 32 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares
-    ../cmd/hypocert -parties 128 -threshold 64 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares
-
 done
