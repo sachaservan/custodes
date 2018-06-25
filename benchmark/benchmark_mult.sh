25fcdc7e4d5e358eb08565990bec71a22d0f1576
ezg <emanuel.zgraggen@gmail.com>
2018-06-25 10:32:05 -0400

    update

--- benchmark/benchmark_mult.sh
+++ benchmark/benchmark_mult.sh
@@ -8,8 +8,8 @@ for i in {0..1} ; do
     echo "$i"
     ../cmd/hypocert -parties 4 -threshold 2 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares -justmult
     ../cmd/hypocert -parties 8 -threshold 4 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares -justmult
-    ../cmd/hypocert -parties 16 -threshold 8 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares  -justmult
-    ../cmd/hypocert -parties 32 -threshold 16 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares  -justmult
-    ../cmd/hypocert -parties 64 -threshold 32 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares  -justmult
-    ../cmd/hypocert -parties 128 -threshold 64 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares  -justmult
+    ../cmd/hypocert -parties 16 -threshold 8 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares -justmult
+    ../cmd/hypocert -parties 32 -threshold 16 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares -justmult
+    ../cmd/hypocert -parties 64 -threshold 32 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares -justmult
+    ../cmd/hypocert -parties 128 -threshold 64 -rootdir "/data/ez/go/src/hypocert" -netlat 0 -runid "$i" -shares -justmult
 done
