880006a8e4d0d4d23868986a4b0ee29d0b4eee3f
sachaservan <sachaservanschreiber@gmail.com>
2018-08-30 19:01:41 -0400

    fix script

--- cmd/scripts/benchmark_paillier.sh
+++ cmd/scripts/benchmark_paillier.sh
@@ -1,12 +1,11 @@
-cd ../cmd
+cd ../
 make
 
-cd ../benchmark
-
+cd ../cmd/bin
 
 for i in {0..0} ; do
     echo "$i"
-    ../cmd/hypocert -parties 4 -threshold 2 -rootdir "/data/ez/go/src/hypocert" -netlat 1 -runid "$i" -save
-    ../cmd/hypocert -parties 8 -threshold 4 -rootdir "/data/ez/go/src/hypocert" -netlat 1 -runid "$i" -save
-    ../cmd/hypocert -parties 16 -threshold 8 -rootdir "/data/ez/go/src/hypocert" -netlat 1 -runid "$i" -save
+    ./hypocert -parties 4 -threshold 2 -rootdir "/data/ez/go/src/hypocert" -netlat 1 -runId "$i" -save
+    ./hypocert -parties 8 -threshold 4 -rootdir "/data/ez/go/src/hypocert" -netlat 1 -runId "$i" -save
+    ./hypocert -parties 16 -threshold 8 -rootdir "/data/ez/go/src/hypocert" -netlat 1 -runId "$i" -save
 done
