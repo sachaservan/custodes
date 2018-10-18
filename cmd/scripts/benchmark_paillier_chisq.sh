7285b11da8c9bfe2eba58df6b0d858fd5ccd6f67
sachaservan <sachaservanschreiber@gmail.com>
2018-10-18 12:46:13 -0400

    add script to only run chisq

---
+++ cmd/scripts/benchmark_paillier_chisq.sh
@@ -0,0 +1,11 @@
+cd ../
+make
+
+cd ../cmd/bin
+
+for i in {0..4} ; do
+    echo "$i"
+    ./hypocert -parties 3 -threshold 2 -rootdir "/home/ez/go/src/hypocert" -netlat 1 -runId "$i" -save -chisqtest
+    ./hypocert -parties 5 -threshold 3 -rootdir "/home/ez/go/src/hypocert" -netlat 1 -runId "$i" -save -chisqtest
+    ./hypocert -parties 10 -threshold 6 -rootdir "/home/ez/go/src/hypocert" -netlat 1 -runId "$i" -save -chisqtest
+done
